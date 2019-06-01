3b1228bc39f39bc61a2ebecb0675977b55179e32
c0derzer0 <contactme.teja@gmail.com>
2019-06-01 15:31:26 -0400

    Add methods to use RSPN module of SPFlow

---
+++ Rspn_read_data.py
@@ -0,0 +1,65 @@
+
+import arff
+import numpy as np
+import pandas as pd
+
+
+def read_arff_data(file_path):
+    with open(file_path) as f:
+        dataDict = arff.load(f)
+        f.close()
+
+    data = np.array(dataDict['data'])
+
+    data = data[:, :-1]
+    data = data.astype(np.float)
+    print(data)
+
+    return data
+
+
+def read_csv_data(file_path):
+    df = pd.read_csv(file_path, sep=',')
+    # df = df.fillna(df.mean())
+    df = df.iloc[:]
+
+    # print(df.dtypes)
+    df = df.convert_objects(convert_numeric=True)
+    # pd.to_numeric(df)
+    # print(df)
+
+    data = df.values
+    print(data.dtype)
+
+    data = data.astype(float)
+
+    data = data[:, 0:-1]
+    print(data)
+
+    return data
+
+
+def read_varying_seq_data(file_path):
+    with open(file_path) as file:
+
+        bulk = file.readlines()
+        data_list = []
+        seq = []
+        for line in bulk:
+            # when the whole sequence ends
+            if line.startswith('\n'):
+                data_list.append(seq)
+                seq = []
+            else:
+                time_step = line.split()
+                for col_val in time_step:
+                    seq.append(col_val)  # add each value of each time step of the whole sequence to a seq list
+
+    for i, seq in enumerate(data_list):
+        for j, val in enumerate(seq):
+            data_list[i][j] = float(val)
+
+    data = [np.reshape(np.array(xi), (1, -1)) for xi in data_list]
+    print(data[0:5])
+
+    return data
---
+++ Rspn_run.py
@@ -0,0 +1,124 @@
+import sys
+sys.path.append('path/to/SPFlow')
+from Rspn_final import *
+from spn.structure.leaves.parametric.Parametric import Gaussian, In_Latent
+from spn.algorithms.RSPN import RSPN
+from spn.structure.Base import Context
+from spn.algorithms.oSLRAU import oSLRAU, oSLRAUParams
+from spn.io.Graphics import plot_spn
+
+
+def run_rspn(dataset, num_variables, num_latent_variables, num_latent_values, unroll, oSLRAU_params,
+             update_after_no_min_batches, full_update, update_leaves, len_sequence_varies):
+
+    data = get_data(dataset)
+
+    from sklearn.model_selection import train_test_split
+    train_data, test_data = train_test_split(data, test_size=0.33, random_state=42)
+    print("train:", len(train_data))
+    print("test:", len(test_data))
+
+    rspn = RSPN(num_variables=num_variables, num_latent_variables=num_latent_variables,
+                num_latent_values=num_latent_values)
+
+    # make first mini_batch from data
+    mini_batch_size = 10
+    first_mini_batch = train_data[0:mini_batch_size]
+
+    dat = first_mini_batch
+    dat = dat[:, 0:num_variables]
+    n = dat.shape[1]  # num of variables in each time step
+    print(n)
+    context = [Gaussian] * n
+    ds_context = Context(parametric_types=context).add_domains(first_mini_batch[:, 0:num_variables])
+
+    spn, initial_template_spn, top_spn = rspn.build_initial_template(first_mini_batch, ds_context,
+                                                                     len_sequence_varies)
+
+    plot_spn(spn, 'rspn_initial_spn.pdf')
+    plot_spn(initial_template_spn, 'rspn_initial_template_spn.pdf')
+    plot_spn(top_spn, 'rspn_top_spn.pdf')
+
+    print(np.mean(rspn.log_likelihood(test_data, unroll, len_sequence_varies)))
+
+    # Determine number of mini batches
+    if not len_sequence_varies:
+        no_of_minibatches = int(train_data.shape[0] / mini_batch_size)
+
+    else:
+        no_of_minibatches = int(len(train_data) / mini_batch_size)
+
+    for i in range(1, no_of_minibatches):
+        mini_batch = train_data[i * mini_batch_size: (i+1) * mini_batch_size]
+
+        update_template = False
+        if i % update_after_no_min_batches == 0:
+            print(i)
+            update_template = True
+
+        template_spn = rspn.learn_rspn(mini_batch, update_template, oSLRAU_params, unroll, full_update, update_leaves,
+                                       len_sequence_varies)
+        
+    plot_spn(template_spn, 'rspn_final_template.pdf')
+    print(np.mean(rspn.log_likelihood(test_data, unroll, len_sequence_varies)))
+
+    unrolled_rspn_full = rspn.get_unrolled_rspn(rspn.get_len_sequence())
+    plot_spn(unrolled_rspn_full, 'rspn_unrolled_full.pdf')
+
+    unrolled_rspn = rspn.get_unrolled_rspn(2)
+    plot_spn(unrolled_rspn, 'rspn_unrolled_2.pdf')
+
+
+def get_data(dataset):
+
+    csv_file_path_libras = 'path/to/file'
+    csv_file_path_hill_valley = 'path/to/file'
+    arff_file_path_eeg_eye = 'path/to/file'
+
+    varying_seq_file_path_japan_vowels = 'path/to/file'
+
+    if dataset == 'libras':
+        file_path = csv_file_path_libras
+        data = read_csv_data(file_path)
+        return data
+
+    elif dataset == 'hill_valley':
+        file_path = csv_file_path_hill_valley
+        data = read_csv_data(file_path)
+        return data
+
+    elif dataset == 'eeg_eye':
+        file_path = arff_file_path_eeg_eye
+        data = read_arff_data(file_path)
+        return data
+
+    elif dataset == 'japan_vowels':
+        file_path = varying_seq_file_path_japan_vowels
+        data = read_varying_seq_data(file_path)
+        return data
+
+    
+
+
+def main():
+    dataset = 'libras'
+    num_variables = 1
+    num_latent_variables = 2
+    num_latent_values = 2
+    unroll = 'backward'
+    oSLRAU_params = oSLRAUParams(mergebatch_threshold=10, corrthresh=0.1, mvmaxscope=1, equalweight=True,
+                                 currVals=True)
+    update_after_no_min_batches = 1
+    full_update = False
+    update_leaves = True
+    len_sequence_varies = False
+
+    run_rspn(dataset, num_variables, num_latent_variables, num_latent_values, unroll, oSLRAU_params,
+             update_after_no_min_batches, full_update, update_leaves, len_sequence_varies)
+
+
+
+
+if __name__ == "__main__":
+
+    main()
