7c9ae5679ba4ceeebc5f07b983ce4d09a1b3ee75
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2023-09-15 20:01:15 +0100

    mixed_hmm starter code

--- mixed_state_hmm.py
+++ mixed_state_hmm.py
@@ -1,26 +1,36 @@
 import numpy as np
 
-class State():
-
-    def __init__(self):
-        self.dist=None
-    
-    def emission(self, length):
-        return [self.dist() for _ in range(length)]
-
-class GaussianState(State):
+class GaussianState():
 
     def __init__(self, mean, std):
         self.mean = mean
         self.std = std
-        self.dist = float(np.random.normal(self.mean, self.std, 1))
+    
+    def sample1(self):
+        return float(np.random.normal(self.mean, self.std, 1))
 
 
-class BernoulliState(State):
+
+class BernoulliState():
 
     def __init__(self, prob):
         self.p = prob
-        self.dist = float(np.random.choice([0, 1], 1, p=[1-prob, prob]))
+    
+    def sample1(self):
+        float(np.random.choice([0, 1], 1, p=[1-self.prob, self.prob]))
+
+
+class MixedState():
+
+    def __init__(self, mean, std, prob):
+        self.mean = mean
+        self.std = std
+        self.p = prob
+        self.normal_state = GaussianState(self.mean, self.std)
+        self.bern_state = BernoulliState(self.p)
+    
+    def sample1(self):
+        return [self.normal_state.sample1(), self.bern_state()]
 
 
 
